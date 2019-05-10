1fe900ce2217baa1b8a35de279e73e080e066ca9
Alessandro Guttrof <wersandrog@gmail.com>
2019-05-10 22:08:16 +0200

    Add Main Menu, Pause Menu and Hub World

---
+++ MegaPoolParty/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    
+
+    private bool m_isAxisInUse = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Cancel") == 1 && m_isAxisInUse == false)
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+
+            m_isAxisInUse = true;
+        }
+        if(Input.GetAxis("Cancel") == 0)
+        {
+            m_isAxisInUse = false;
+        }
+    }
+
+    void Resume()
+    {
+
+    }
+
+    void Pause()
+    {
+
+    }
+}
