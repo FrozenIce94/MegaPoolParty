062217cf8efef7901679f214ad6d92a4c003d5a8
FrozenIce94 <Flochen16@aol.com>
2019-05-12 11:28:35 +0200

    Animation Hub

---
+++ MegaPoolParty/Assets/Scripts/Hub/PlaySound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioSource teleport, wooooosh, fall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayTeleportSound()
+    {
+        teleport.Play();
+    }
+
+    public void PlayFallSound()
+    {
+        fall.Play();
+    }
+
+    public void PlayWooooshSound()
+    {
+        wooooosh.Play();
+    }
+}
